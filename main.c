d941c25ec6b444a4ae3aee97ca68b511a866b4b0
Tu Nombre Completo <tu_correo@universidad.edu>
2026-05-19 11:29:58 -0500

    feat: agregar archivo cabecera calificaciones.h y función de ingreso

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "calificaciones.h"
+
+int main()
+{
+    int n;
+    printf("Ingrese el numero de estudiantes: ");
+    scanf("%d", &n);
+
+    float calificaciones[n][ASIGNATURAS];
+
+    return 0;
+}
