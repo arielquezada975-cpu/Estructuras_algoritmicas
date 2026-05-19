d1f81ef609b83771f065910a5628075dc2cf20ba
Annet Paez <annet.paez@udla.edu.ec>
2026-05-19 11:35:14 -0500

    estructura base y archivo cabecera

---
+++ calificaciones.h
@@ -0,0 +1,13 @@
+    #ifndef CALIFICACIONES_H
+#define CALIFICACIONES_H
+
+#define ASIGNATURAS 3
+
+void ingresarCalificaciones(float *calificaciones, int n);
+void promedioEstudiante(float *calificaciones, int n);
+void promedioAsignatura(float *calificaciones, int n);
+void maxMinEstudiante(float *calificaciones, int n);
+void maxMinAsignatura(float *calificaciones, int n);
+void aprobadosReprobados(float *calificaciones, int n);
+
+#endif
