d44e4a59b86dd8e73ceedd83d0376c78d9478d08
Tu Nombre Completo <tu_correo@universidad.edu>
2026-05-19 12:06:26 -0500

    chore: integrar todas las funciones en main.c y resolver conflictos

--- main.c
+++ main.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 #include "calificaciones.h"
 
-int main()
-{
+int main() {
     int n;
+    printf("=== SISTEMA DE GESTIÓN DE CALIFICACIONES ===\n");
     printf("Ingrese el numero de estudiantes: ");
     scanf("%d", &n);
 
     float calificaciones[n][ASIGNATURAS];
 
+    // Ingreso y validación
+    ingresarCalificaciones(&calificaciones[0][0], n);
+    
+    // Cálculos y reportes
+    promedioEstudiante(&calificaciones[0][0], n);
+    promedioAsignatura(&calificaciones[0][0], n);
+    maxMinEstudiante(&calificaciones[0][0], n);
+    maxMinAsignatura(&calificaciones[0][0], n);
+    aprobadosReprobados(&calificaciones[0][0], n);
+
+    printf("\n✅ Proceso finalizado.\n");
     return 0;
 }
