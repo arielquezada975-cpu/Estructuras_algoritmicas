d87cfddfcfdf4918ef29862d3e508e5fb9920cdd
Annet Paez <annet.paez@udla.edu.ec>
2026-05-19 11:50:10 -0500

    calculo promedio por estudiante

---
+++ Calculo_promedio_por_estudiante.c
@@ -0,0 +1,11 @@
+void promedioEstudiante(float *cal, int n)
+{
+    printf("\n--- PROMEDIO POR ESTUDIANTE ---\n");
+    for (int i = 0; i < n; i++)
+    {
+        float suma = 0;
+        for (int j = 0; j < ASIGNATURAS; j++)
+            suma += *(cal + i * ASIGNATURAS + j);
+        printf("Estudiante %d -> Promedio: %.2f\n", i + 1, suma / ASIGNATURAS);
+    }
+}
